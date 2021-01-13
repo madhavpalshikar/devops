8956d365c5aadd679cfb6cfc2495243c113e0703
Madhav <mpdebug200@gmail.com>
2021-01-13 22:17:20 +0530

    redis-3nodes done

--- redis-3nodes/service1/app.js
+++ redis-3nodes/service1/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var redis = require("redis");
-var subscriber = redis.createClient();
+var subscriber = redis.createClient({host: 'redis'});
 subscriber.on("message", function (channel, message) {
  console.log("Message: " + message + " on channel: " + channel + " is arrive!");
 });
